d97ae0897ba3286e70c377a5ea93457b5b7cb37f
Axel Arroyo <axel.arroyo@sansano.usm.cl>
2022-06-29 17:15:21 -0400

    user model updated, bcrypt password

--- models/user.js
+++ models/user.js
@@ -5,6 +5,15 @@ const UserSchema = new mongoose.Schema({
     password: {type: String, required: true},
     name: {type: String, required: true},
     role: {type: String, required: true},
+    run: {type: String, required: false},
+    address: {type: String, required: false},
+    birthday: {type: Date, required: false},
+    phone: {type: String, required: false},
+    region: {type: String, required: false},
+    province: {type: String, required: false},
+    commune: {type: String, required: false},
+    sex: {type: String, required: false},
+
     createdAt: {
         type: Date,
         default: Date.now
@@ -12,14 +21,7 @@ const UserSchema = new mongoose.Schema({
     updatedAt: {
         type: Date,
         default: Date.now
-    },
-    cart: [{
-        productId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Product'
-        },
-        quantity: Number
-    }]
+    }
 });
 
 
@@ -34,7 +36,34 @@ UserSchema.statics.login = async function (email,password){
         }
         reject('Incorrect email')
     })
-    
 }
+
+UserSchema.statics.createUser = async function ({email, password, name, run, address, birthday, phone, region, province, commune, sex, role}){
+    const user = await this.findOne({email})
+    return new Promise ((resolve, reject) => {
+        if (user) {
+            reject('User already exists')
+            return;
+        }
+        const newUser = new this({
+            email,
+            password,
+            name,   
+            run,
+            address,
+            birthday,
+            phone,
+            region,
+            province,
+            commune,
+            sex,
+            role
+        })
+        newUser.save()
+        resolve(newUser)
+    })
+}
+
+
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,43 +2,58 @@ const express = require('express')
 const router = express.Router();
 const User = require('../models/user')
 const emailExistence = require('email-existence');
+const bcrypt = require('bcrypt')
 
 const validateEmail = async (email) => {
-    let res = await emailExistence.check(email, function (err, res) {
-        if (res) {
-            return true
-        }
-        else {
-            return false
-        }
-    });
+    return new Promise((resolve, reject) => {
+        emailExistence.check(email, function (error, response) {
+            if (error) {
+                reject(error)
+            } else {
+                resolve(response)
+            }
+        });
+    })
 }
 
 router.post('/register', async (req, res) => {  
     try {
-        const { email, password, name } = req.body
-        const emailValid = await validateEmail(email)
+        const { email, password, name, run, address, birthday, phone, region, province, commune, sex, role } = req.body
+        const emailValid = await validateEmail(email);
         if(!emailValid) {
             return res.status(400).json({
                 message: 'Invalid email'
             })
         }
-        const user = await User.findOne({ email })
-        if(user) {
-            return res.status(400).json({
-                message: 'User already exists'
+        // hash the password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+        // create the user
+        const birthdayDate = new Date(birthday);
+        await User.createUser({
+            email,
+            password: hashedPassword,
+            name,
+            run,
+            address,
+            birthday: birthdayDate,
+            phone,
+            region,
+            province,
+            commune,
+            sex,
+            role,
+        }).then(user => {
+            return res.status(201).json({
+                message: 'User created',
+                user
             })
         }
-        // const newUser = new User({
-        //     email,
-        //     password,
-        //     name
-        // })
-        // const savedUser = await newUser.save()
-        // return res.status(201).json({
-        //     message: 'User created',
-        //     user: savedUser
-        // })
+        ).catch(err => {
+            return res.status(400).json({
+                message: err,
+            })
+        })
     } catch (error) {
         return res.status(500).json({
             message: error.message
