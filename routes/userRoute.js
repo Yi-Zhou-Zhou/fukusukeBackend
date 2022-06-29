23415dc5342030959f975da15a0d23952d950276
Axel Arroyo <axel.arroyo@sansano.usm.cl>
2022-06-29 16:56:57 -0400

    router changed, check if email exists

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,42 +1,66 @@
 const express = require('express')
 const router = express.Router();
 const User = require('../models/user')
+const emailExistence = require('email-existence');
 
-router.route('/').post(async (req,res) => {
-    
-    const action = req.body.action;
-    if (action == "register"){
-        const email = req.body.email;
-        const password = req.body.password;
-        const name = req.body.name;
-        const role = req.body.role;
-        const newuser = new User({
-        email, password, name, role
+const validateEmail = async (email) => {
+    let res = await emailExistence.check(email, function (err, res) {
+        if (res) {
+            return true
+        }
+        else {
+            return false
+        }
+    });
+}
+
+router.post('/register', async (req, res) => {  
+    try {
+        const { email, password, name } = req.body
+        const emailValid = await validateEmail(email)
+        if(!emailValid) {
+            return res.status(400).json({
+                message: 'Invalid email'
+            })
+        }
+        const user = await User.findOne({ email })
+        if(user) {
+            return res.status(400).json({
+                message: 'User already exists'
+            })
+        }
+        // const newUser = new User({
+        //     email,
+        //     password,
+        //     name
+        // })
+        // const savedUser = await newUser.save()
+        // return res.status(201).json({
+        //     message: 'User created',
+        //     user: savedUser
+        // })
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
         })
-
-        newuser.save()
     }
+});
 
-    else if (action == "login") {
-        try {
-            const email = req.body.email
-            const password = req.body.password
-            const user = await User.login(email,password)
-            return res.status(200).send({
-                user
-            })
-
-        }
-        catch(error){
-            
-            return res.status(400).send({
-                ok: false,
-                err: {
-                    message: error
-                }
-            })
-        }
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const user = await User.login(email, password)
+        return res.status(200).send({
+            user
+        })
+    } catch (error) {
+        return res.status(400).send({
+            ok: false,
+            err: {
+                message: error
+            }
+        })
     }
+});
 
-})
 module.exports = router;
