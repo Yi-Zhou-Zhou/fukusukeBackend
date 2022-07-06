9303c2c8a88715fb55e0ac49c2f19c5cca143c1f
Axel Arroyo <axel.arroyo@sansano.usm.cl>
2022-07-05 22:47:06 -0400

    app use product route

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ app.use(express.json())
 
 mongoose.connect('mongodb+srv://www-chipleki:Kp6LGRV4t7AUBfXV@cluster0.5pgu7.mongodb.net/fukusuke', {useNewUrlParser: true, useUnifiedTopology: true});
 
-app.use('/', require('./routes/userRoute'));
+app.use('/user', require('./routes/userRoute'));
+app.use('/product', require('./routes/productRoute'));
 
 app.listen(8080, function(){
     console.log("Servidor Iniciado");
